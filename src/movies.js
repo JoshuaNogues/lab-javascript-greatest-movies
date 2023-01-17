44e045ea51e98693ac008186b8d4977e55a1c313
JoshuaNogues <120759313+JoshuaNogues@users.noreply.github.com>
2023-01-17 17:39:21 -0500

    done

--- src/movies.js
+++ src/movies.js
@@ -50,17 +50,33 @@ function dramaMoviesScore(moviesArray) {
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear(moviesArray) {
-    return [...moviesArray].sort((a, b)=> {
-        if(a.year === b.year) {
-            return 
-        }
-    })
-}
+    const newArr = moviesArray.map((movie) => {
+      return movie;
+    });
+  
+    newArr.sort((a, b) => {
+      if (a.year === b.year) {
+        return a.title.localeCompare(b.title);
+      }
+      return a.year - b.year;
+    });
+    return newArr;
+  }
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically(moviesArray) {
-    
-}
+function orderAlphabetically(array) {
+    const newArrTwo = array.map((movie) => {
+      return movie.title;
+    });
+  
+    // console.log(twentyTitles);
+    newArrTwo.sort((a, b) => {
+      return a.localeCompare(b);
+    });
+    const twentyTitles = newArrTwo.slice(0, 20);
+  
+    return twentyTitles;
+  }
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 function turnHoursToMinutes(moviesArray) {}
